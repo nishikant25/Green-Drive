6707f7720885b68ede2ce2c16a82d315b1cb5674
nishikant25 <nishikantgavali5@gmail.com>
2026-04-05 18:32:22 +0530

    Implement cost calculation for different fuel types
    Added a calculator function to compute costs for EV, petrol, and diesel based on distance.

--- script.js
+++ script.js
@@ -2,4 +2,18 @@ function scrollToSection(){
 document.getElementById("features").scrollIntoView({behavior:"smooth"});
 }
 
+// 🔥 CALCULATOR FUNCTION
+function calculateCost(){
+let distance = document.getElementById("distance").value;
+
+let ev = distance * 1;
+let petrol = distance * 7;
+let diesel = distance * 6;
+
+// update UI
+document.getElementById("evCost").innerText = "₹ " + ev;
+document.getElementById("petrolCost").innerText = "₹ " + petrol;
+document.getElementById("dieselCost").innerText = "₹ " + diesel;
+}
+
 console.log("Premium EV Site Loaded 🚀");
